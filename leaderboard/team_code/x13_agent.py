4a033ed66d13e06a14594625fe22afea010d602b
pagand <pagand@sfu.ca>
2023-03-14 15:14:20 -0700

    fixed

--- leaderboard/team_code/x13_agent.py
+++ leaderboard/team_code/x13_agent.py
@@ -288,7 +288,6 @@ class x13Agent(autonomous_agent.AutonomousAgent):
 		# encoding.append(self.net.image_encoder(list(self.input_buffer['rgb'])))
 
 		depth = torch.from_numpy(np.array(rgb_to_depth(scale_and_crop_image_cv(swap_RGB2BGR(tick_data['depth']), scale=self.config.scale, crop=self.config.input_resolution))))
-		torch.save(depth,'depth.pt')
 		self.input_buffer['depth'] = depth.to('cuda', dtype=torch.float32)
 		# self.input_buffer['depth'].popleft()
 		# self.input_buffer['depth'].append(depth.to('cuda', dtype=torch.float32))

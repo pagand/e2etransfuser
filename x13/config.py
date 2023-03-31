69af341aa788c416b0e1fe5177d44682d952a304
mmahdavi <mmahdavi@solar.planets>
2023-03-31 16:27:17 -0700

    update

--- x13/config.py
+++ x13/config.py
@@ -4,8 +4,8 @@ import random
 class GlobalConfig:
     num_worker = 0# for debugging 0
     wandb = True
-    gpu_id = '0'
-    model = 'march31_effnet_redl_concat_solar_debug'
+    gpu_id = '3'
+    model = 'march31_effnet_redl_concat_solar'
     wandb_name = model 
     logdir = 'log/'+model
     model = 'randomized_low_data' # for wandb

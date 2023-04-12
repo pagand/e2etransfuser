fb25e17ed82d9767ab389bc28b1ef261928b8d0a
Pedram Agand <pagand@sfu.ca>
2023-04-12 16:31:24 -0700

    Update config.py

--- x13/config.py
+++ x13/config.py
@@ -68,7 +68,8 @@ class GlobalConfig:
 
     # root_dir = '/home/aisl/OSKAR/Transfuser/transfuser_data/14_weathers_full_data'  #14_weathers_full_data OR clear_noon_full_data
     #root_dir = '/localhome/pagand/projects/e2etransfuser/data'  # for the CVPR dataset
-    root_dir = '/home/mohammad/Mohammad_ws/autonomous_driving/transfuser/data'#  '/localscratch/mmahdavi/transfuser/data' #  for the PAMI dataset
+    #root_dir = '/home/mohammad/Mohammad_ws/autonomous_driving/transfuser/data'#  '/localscratch/mmahdavi/transfuser/data' #  for the PAMI dataset
+    root_dir = '/localhome/pagand/projects/e2etransfuser/transfuser_pmlr/data' # for the PAMI dataset
 
     train_data, val_data = [], []
 

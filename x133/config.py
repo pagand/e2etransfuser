2cdb818ad2df4c07578936bef3c8d5dfc15fc0d2
mohammad mahdavian <65621717+mmahdavian@users.noreply.github.com>
2023-05-01 15:17:26 -0700

    Update config.py

--- x133/config.py
+++ x133/config.py
@@ -25,8 +25,8 @@ class GlobalConfig:
     pred_len = 3 # future waypoints predicted
 
     # root_dir = '/home/aisl/OSKAR/Transfuser/transfuser_data/14_weathers_full_data'  #14_weathers_full_data OR clear_noon_full_data
-    # root_dir = '/localhome/pagand/projects/e2etransfuser/data'  # for the CVPR dataset
-    root_dir = '/home/mohammad/Mohammad_ws/autonomous_driving/transfuser/data'  # for the PMLR dataset
+    root_dir = '/localhome/pagand/projects/e2etransfuser/data'  # for the CVPR dataset
+    #root_dir = '/home/mohammad/Mohammad_ws/autonomous_driving/transfuser/data'  # for the PMLR dataset
 
     train_data, val_data = [], []
 

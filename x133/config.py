b22d16134b2c7eba98e25cc95641923194a00ba5
Pedram Agand <pagand@sfu.ca>
2023-05-01 15:41:52 -0700

    Update config.py

--- x133/config.py
+++ x133/config.py
@@ -24,9 +24,8 @@ class GlobalConfig:
     seq_len = 1 # jumlah input seq
     pred_len = 3 # future waypoints predicted
 
-    # root_dir = '/home/aisl/OSKAR/Transfuser/transfuser_data/14_weathers_full_data'  #14_weathers_full_data OR clear_noon_full_data
-    root_dir = '/localhome/pagand/projects/e2etransfuser/data'  # for the CVPR dataset
     #root_dir = '/home/mohammad/Mohammad_ws/autonomous_driving/transfuser/data'  # for the PMLR dataset
+    root_dir = '/localhome/pagand/projects/e2etransfuser/transfuser_pmlr/data'
 
     train_data, val_data = [], []
 
